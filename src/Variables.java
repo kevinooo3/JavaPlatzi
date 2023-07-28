bf8edfe01409bc25ee9fa084d16fabd1ec7fc590
kevinooo <kevinooo_18@hotmail.com>
2023-07-28 16:52:36 -0500

    regrese como estaba antes de realizar las pruebas git.

--- src/Variables.java
+++ src/Variables.java
@@ -15,10 +15,6 @@ public class Variables {
         var entero = 12;
         entero = entero/5;
         System.out.println(entero + "AJSDSAKDAJ"); // CONVIERTE A ENTERO EN UNA CADENA Y NO PASA NADA CAUSA
-        int gaaa;
-        String prueba_de_commits ="Esta es una prueba de commits";
-        int gaaax ;
-        int tercergato = 1212;
-        int cuartocontacto = 11;
+
     }
 }
