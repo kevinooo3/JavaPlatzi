60eb3b908fd522469091940f25d41b7dcad753c7
kevinooo <kevinooo_18@hotmail.com>
2023-07-28 12:05:55 -0500

    1era emterrada

---
+++ src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int j = 3;
+        j++;
+        System.out.println(j); // imprime 4
+        j--;
+        System.out.println(j); // imprime 3
+        // valor actual j
+        System.out.println("valor actual " + j);
+        // sufijo
+        System.out.println("sufijo resta "+ (100+ j--)); // imprime el ultimo valor de j , no le importa si poner mas o menos cuando usas como variable.
+        System.out.println("sufijo summa "+ (100+ j++)); // imprime el ultimo valor de j , no le importa si poner mas o menos cuando usas como variable.
+        // prefijo
+        System.out.println("prefijo summa "+ (100+ ++j));
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args) {
+        int numeroInt = 1111111111;
+        long numeroLong = 11111111111111111L; // long necesita al final L
+
+        double numeroDouble = 123.456;
+        float numeroFloat = 123.456F; //FLOAT NECESITA AL FINAL F
+        char letra = 'a';
+        String palabra ="La letra es crema";
+
+        //System.out.println(palabra);
+        /*
+        * Apartir de jdk 11 uno puede usar var , q es un tipo de variable que se transforma en lo q sea.
+        * */
+        var entero = 12;
+        entero = entero/5;
+        System.out.println(entero+"AJSDSAKDAJ"); // CONVIERTE A ENTERO EN UNA CADENA Y NO PASA NADA CAUSA
+
+    }
+}
